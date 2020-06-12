f8ffaf3d7d4406988dda148932cdbb4f46c5f1ed
Nana Adjei Manu <claeusdev@gmail.com>
2020-06-12 02:08:07 +0000

    finish initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { CSSTransition } from "react-transition-group";
+import "./App.scss";
 
+function Toast({ message, type, duration }) {
+  const [active, setActive] = useState(true);
+
+  return (
+    <CSSTransition
+      in={active}
+      timeout={duration}
+      classNames="toast"
+      unmountOnExit
+      onExit={() => setActive(false)}
+    >
+      <div className={`toast ${type ? type : "default"} `}>
+        <div className="toast__message">{message}</div>
+        <button className="toast__dismiss" onClick={() => setActive(false)}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
+  );
+}
 function App() {
+  const [active, setActive] = useState(false);
+  const [type, setType] = useState("default");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select
+        id="lang"
+        onChange={({ target }) => {
+          setType(target.value);
+          setActive(false);
+        }}
+        value={type}
+      >
+        <option value="default">Select a type</option>
+        <option value="warning">Warning</option>
+        <option value="success">Success</option>
+        <option value="error">Error</option>
+      </select>
+      <button
+        className="button"
+        onClick={() => {
+          console.log("here");
+          setActive(true);
+        }}
+      >
+        Show Toast
+      </button>
+
+      {active ? (
+        <Toast type={type} message="Warning message" duration={3000} />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
