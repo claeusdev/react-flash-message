622039f67b4d2ca437e29c43d17fcf8fd39f2c3a
Nana Adjei Manu <claeusdev@gmail.com>
2020-06-13 18:16:33 +0000

    move toast to component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import React, { useState } from "react";
-import { CSSTransition } from "react-transition-group";
+import Toast from "./Toast";
 import "./App.scss";
 
-function Toast({ message, type, duration }) {
-  const [active, setActive] = useState(true);
-
-  return (
-    <CSSTransition
-      in={active}
-      timeout={duration}
-      classNames="toast"
-      unmountOnExit
-      onExit={() => setActive(false)}
-    >
-      <div className={`toast ${type ? type : "default"} `}>
-        <div className="toast__message">{message}</div>
-        <button className="toast__dismiss" onClick={() => setActive(false)}>
-          Dismiss
-        </button>
-      </div>
-    </CSSTransition>
-  );
-}
 function App() {
   const [active, setActive] = useState(false);
   const [type, setType] = useState("default");
